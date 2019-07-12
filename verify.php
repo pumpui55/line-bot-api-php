f1716c5eb16866383ac64496483ac652be73ccd0
pumpui55 <52811519+pumpui55@users.noreply.github.com>
2019-07-12 14:58:31 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'p4YfWb8mnn2TjqTIWuuCLesKgbkGbTpTPXtkoT9oM9/iafVcRtWfgBqo8psrNFxsfg8+YtMvH0xdN3CC4xub7Cuj0iZa7B0nAUhdMk6ks7G4ihcuG1Ln2XgtHwtvtjqpH3LFwO/5Ie8RaXmxlFWM0wdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
